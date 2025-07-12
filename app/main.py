fba9ba810eb56ff665e86d45459c1151ec666ab0
Avinash <avinash@mnhsoftskills.com>
2025-07-12 08:38:29 +0530

    Add log

--- app/main.py
+++ app/main.py
@@ -13,6 +13,12 @@ from app.api.v1.router import api_router
 from fastapi.middleware.cors import CORSMiddleware
 from app.utils.scheduler import start_scheduler
 
+import logging
+logging.getLogger('sqlalchemy.engine').setLevel(logging.ERROR)
+logging.getLogger("uvicorn.access").setLevel(logging.ERROR)
+logging.getLogger("uvicorn.error").setLevel(logging.ERROR)
+logging.getLogger("fastapi").setLevel(logging.ERROR)
+
 app = FastAPI(title="Thrum Backend")
 
 app.add_middleware(SessionIDMiddleware)
