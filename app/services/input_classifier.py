1d828af44760d0ee03ca3155c228e5461441a58a
Avinash <avinash@mnhsoftskills.com>
2025-07-15 20:30:30 +0530

    Update prompt

--- app/services/input_classifier.py
+++ app/services/input_classifier.py
@@ -48,7 +48,7 @@ You are a classification engine for a conversational game assistant.
 last thrum reply: {last_thrum_reply} (This is the reply that Thrum gave to the user's last message)
 """
   
-    print('THRUM_PROMPT.................:', THRUM_PROMPT)
+    print('memory_context_str.................:', memory_context_str)
     
     final_system_prompt =  f"""{THRUM_PROMPT}
 USER MEMORY & RECENT CHAT:
