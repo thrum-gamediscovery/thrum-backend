f364e13e535c740f82d591fde8731761adac23b7
Avinash <avinash@mnhsoftskills.com>
2025-07-16 14:30:40 +0530

    Update schedular

--- app/utils/scheduler.py
+++ app/utils/scheduler.py
@@ -18,9 +18,10 @@ def async_wrapper(coro_func):
     return wrapped
 
 def start_scheduler():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(async_wrapper(check_for_nudge), 'interval', seconds=30, max_instances=1)
-    scheduler.add_job(async_wrapper(recommend_game), 'interval', seconds=5, max_instances=1)
-    scheduler.add_job(async_wrapper(get_followup), 'interval', seconds=10, max_instances=1)
-    scheduler.add_job(async_wrapper(ask_for_name_if_needed), 'interval', seconds=15, max_instances=1)
-    scheduler.start()
+    # scheduler = BackgroundScheduler()
+    # scheduler.add_job(async_wrapper(check_for_nudge), 'interval', seconds=30, max_instances=1)
+    # scheduler.add_job(async_wrapper(recommend_game), 'interval', seconds=5, max_instances=1)
+    # scheduler.add_job(async_wrapper(get_followup), 'interval', seconds=10, max_instances=1)
+    # scheduler.add_job(async_wrapper(ask_for_name_if_needed), 'interval', seconds=15, max_instances=1)
+    # scheduler.start()
+    pass
