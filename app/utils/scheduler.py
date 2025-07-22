a13b4e919d0ea7e424897d31bbd08a062ecb8610
Avinash <avinash@mnhsoftskills.com>
2025-07-22 21:18:07 +0530

    Fixed KAN - 76,77,78,79

--- app/utils/scheduler.py
+++ app/utils/scheduler.py
@@ -20,9 +20,9 @@ def async_wrapper(coro_func):
 
 def start_scheduler():
     scheduler.add_job(async_wrapper(check_for_nudge), 'interval', seconds=25, max_instances=2)
-    scheduler.add_job(async_wrapper(recommend_game), 'interval', seconds=2, max_instances=2)
-    scheduler.add_job(async_wrapper(get_followup), 'interval', seconds=3, max_instances=2)
-    scheduler.add_job(async_wrapper(ask_for_name_if_needed), 'interval', seconds=4, max_instances=2)
+    scheduler.add_job(async_wrapper(recommend_game), 'interval', seconds=5, max_instances=2)
+    scheduler.add_job(async_wrapper(get_followup), 'interval', seconds=7, max_instances=2)
+    scheduler.add_job(async_wrapper(ask_for_name_if_needed), 'interval', seconds=9, max_instances=2)
     scheduler.start()
 
 def stop_scheduler():
